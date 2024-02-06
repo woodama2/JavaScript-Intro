dfdb82cfe2a24ed550a0c0ec098e57cdbefb1d71
Mandy <156150059+woodama2@users.noreply.github.com>
2024-02-06 11:51:13 -0500

    removed exercise 1.1 tasks and added first 10 pokemons on pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-alert('Hello world');
-
-let favoriteFood = 'icecream';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+    {name: 'Charmander', height: 6, types: ['fire']},
+    {name: 'Diglett', height: 2, types: ['ground']},
+    {name: 'Tangela', height: 10, types: ['grass']},
+    {name: 'Sharpedo', height: 18, types: ['dark', 'water']},
+    {name: 'Smoochum', height: 4, types: ['psychic', 'ice']},
+    {name: 'Squirtle', height: 5, types: ['water']},
+    {name: 'Pikachu', height: 4, types: ['electric']},
+    {name: 'Starmie', height: 11, types: ['psychic', 'water']},
+    (name: 'Caterpie', height: 3, types: ['bug'])
+];
