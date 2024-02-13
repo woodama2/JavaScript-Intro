54d714024169f33c07948d54084603fb4a7d1459
Mandy <156150059+woodama2@users.noreply.github.com>
2024-02-13 17:42:38 -0500

    DOM Manipulation, changes to HTML, CSS, & JavaScript for Pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,65 @@
-let pokemonRepository = (function(){
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
+    { name: "Charmander", height: 6, types: ["fire"] },
+    { name: "Diglett", height: 2, types: ["ground"] },
+    { name: "Tangela", height: 10, types: ["grass"] },
+    { name: "Sharpedo", height: 18, types: ["dark", "water"] },
+    { name: "Smoochum", height: 4, types: ["psychic", "ice"] },
+    { name: "Squirtle", height: 5, types: ["water"] },
+    { name: "Pikachu", height: 4, types: ["electric"] },
+    { name: "Starmie", height: 11, types: ["psychic", "water"] },
+    { name: "Caterpie", height: 3, types: ["bug"] },
+  ];
 
-let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    { name: 'Charmander', height: 6, types: ['fire'] },
-    { name: 'Diglett', height: 2, types: ['ground'] },
-    { name: 'Tangela', height: 10, types: ['grass'] },
-    { name: 'Sharpedo', height: 18, types: ['dark', 'water'] },
-    { name: 'Smoochum', height: 4, types: ['psychic', 'ice'] },
-    { name: 'Squirtle', height: 5, types: ['water'] },
-    { name: 'Pikachu', height: 4, types: ['electric'] },
-    { name: 'Starmie', height: 11, types: ['psychic', 'water'] },
-    { name: 'Caterpie', height: 3, types: ['bug'] }
-];
-
-function add(pokemon) {
-    if (typeof pokemon === 'object') {
-        pokemonList.push(pokemon);    
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      pokemonList.push(pokemon);
     }
-}
+  }
 
-function getAll(){
+  function getAll() {
     return pokemonList;
-}
+  }
 
-return {
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  function addEventListener(event) {
+    let button = document.querySelector(".pokemon-button");
+    let listItem = document.querySelector(li);
+  }
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("pokemon-button");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+
+    button.addEventListener("click", function () {
+      showDetails(pokemon.name);
+    });
+  }
+
+  return {
     add: add,
-    getAll: getAll
-}
+    getAll: getAll,
+    addListItem: addListItem,
+  };
 })();
 
-
-
-// add a new object(pokemon) to the pokemonList
+// // add a new object(pokemon) to the pokemonList
 pokemonRepository.add({
-    name: 'Krabby', height: 4, types: ['water']
+  name: "Krabby",
+  height: 4,
+  types: ["water"],
 });
 
-
 // written using forEach() loop syntax
-// print the list of names from pokemonList and corresponding height of that name
-pokemonRepository.getAll().forEach(function(pokemon) {
-    // print a message if the height is bigger than 15
-    if (pokemon.height > 15) {
-        document.write(pokemon.name + ' \(height\: ' + pokemon.height + '\) \- Wow\, that\'s big!' + '<br>')
-    } else {
-        //print without the message
-        document.write(pokemon.name + ' \(height\: ' + pokemon.height + '\)' + '<br>')
-    }    
-})
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
